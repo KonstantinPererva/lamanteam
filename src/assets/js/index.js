fba379453cd90879b85e15055960a61a582b1158
Kostya <pererva.kostya@outlook.com>
2020-11-16 18:51:10 +0200

    реализация слайдера

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -122,6 +122,110 @@ window.addEventListener('load', function () {
     setTimeout(function () {
         document.body.style.opacity = '1';
         document.body.style.transition = '0';
-    },100)
+    },100);
+
+    function initMainSlider() {
+        var sliderMainPicture = document.querySelector('.banner-slider');
+        var slider_2;
+        var initialSlide = 0;
+
+        if (sliderMainPicture) {
+            slider_2 = new Swiper(sliderMainPicture, {
+                speed: 1000,
+                slidesPerView: 1,
+                loop: true,
+                effect: "fade",
+                autoplay: {
+                    delay: 10000,
+                    paused: false,
+                    disableOnInteraction: false
+                },
+                // navigation: {
+                //     prevEl: ".navigation-button-prev",
+                //     nextEl: ".navigation-button-next"
+                // },
+                on: {
+                    init: function()
+                    {
+                        initElementsAnimation(this);
+                        animateElShow(this.slides[this.params.initialSlide + 1]);
+                    }
+                    ,
+                    slideChange: function ()
+                    {
+                        initElementsAnimation(this);
+                        activeSlide(this, this.slides, this.activeIndex, this.previousIndex);
+                    }
+                }
+            });
+
+            function initElementsAnimation(slider)
+            {
+                var self = this;
+                self.slider = slider;
+
+                console.log(self.slider.slides.length);
+
+                for(let i = 0; i < self.slider.slides.length; i++)
+                {
+                    self.slider.slides[i].elements = [].slice.call(self.slider.slides[i].querySelectorAll('.animation'),0);
+
+                    for(let j = 0; j < self.slider.slides[i].elements.length; j++)
+                    {
+                        self.slider.slides[i].elements[j].style.animationDuration = self.slider.params.speed / 3 + "ms";
+                        self.slider.slides[i].elements[j].classAnimIn = self.slider.slides[i].elements[j].dataset.animatedIn;
+                        self.slider.slides[i].elements[j].classAnimOut = self.slider.slides[i].elements[j].dataset.animatedOut;
+                    }
+                }
+            }
+
+            function animateElShow(slide)
+            {
+                var self = this;
+                self.slide = slide;
+
+                for(let i = 0; i < self.slide.elements.length; i++)
+                {
+                    self.slide.elements[i].classList.remove(self.slide.elements[i].classAnimOut);
+                    self.slide.elements[i].classList.add('animated');
+                    self.slide.elements[i].classList.add(self.slide.elements[i].classAnimIn);
+                }
+            }
+
+            function animateElHide(slide)
+            {
+                var self = this;
+                self.slide = slide;
+
+                for(let i = 0; i < self.slide.elements.length; i++)
+                {
+                    self.slide.elements[i].classList.remove(self.slide.elements[i].classAnimIn);
+                    self.slide.elements[i].classList.add('animated');
+                    self.slide.elements[i].classList.add(self.slide.elements[i].classAnimOut);
+                }
+            }
+
+            function activeSlide(slider,slides,ind,prev)
+            {
+                var _s = this;
+                _s.ind = ind;
+                _s.prev = prev;
+                _s.slides = slides;
+
+                if(_s.prev !== undefined)
+                {
+                    animateElHide(_s.slides[_s.prev]);
+                }
+
+                console.log(slider.params);
+
+                setTimeout(function () {
+                    animateElShow(_s.slides[_s.ind]);
+                },slider.params.speed / 3);
+            }
+        }
+    }
+
+    initMainSlider()
 })
 

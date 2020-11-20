2f221e896f00e833638a62c07850cc7713323dff
Kostya <pererva.kostya@outlook.com>
2020-11-20 15:53:52 +0200

    правки после выкатки на прод

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -48,7 +48,6 @@ function PagePopup(popup) {
 
     _s.open = function () {
         _s.popup.style.display = 'block';
-        console.log(window.innerWidth - document.body.clientWidth);
         document.documentElement.style.width = document.body.clientWidth + 'px';
         document.documentElement.style.overflow = 'hidden';
 
@@ -103,6 +102,8 @@ window.addEventListener('load', function () {
         for (let i = 0; i < listBtnOpenPopup.length; i++) {
             let popup = listBtnOpenPopup[i].dataset.openPopup;
 
+            if (!popup) { continue }
+
             listBtnOpenPopup[i].popup = new PagePopup(popup);
 
             listBtnOpenPopup[i].popup.init();
@@ -117,115 +118,149 @@ window.addEventListener('load', function () {
         new WOW().init();
     }
 
-    $("input[name=phone]").mask("+38 (099) 999-99-99");
-
-    setTimeout(function () {
-        document.body.style.opacity = '1';
-        document.body.style.transition = '0';
-    },100);
+    // $("input[name=phone]").mask("+38 (099) 999-99-99");
 
     function initMainSlider() {
-        var sliderMainPicture = document.querySelector('.banner-slider');
-        var slider_2;
+        var sliderMain;
         var initialSlide = 0;
+        var delay = 4000;
 
-        if (sliderMainPicture) {
-            slider_2 = new Swiper(sliderMainPicture, {
-                speed: 1000,
-                slidesPerView: 1,
-                loop: true,
-                effect: "fade",
-                autoplay: {
-                    delay: 10000,
-                    paused: false,
-                    disableOnInteraction: false
+        sliderMain = new Swiper('.banner-slider', {
+            speed: 1000,
+            slidesPerView: 1,
+            initialSlide: initialSlide,
+            effect: "fade",
+            autoplay: {
+                delay: delay,
+                paused: false,
+                disableOnInteraction: false
+            },
+            // navigation: {
+            //     prevEl: '.navigation-button-prev',
+            //     nextEl: '.navigation-button-next'
+            // },
+            runCallbacksOnInit: false,
+            on: {
+                init: function()
+                {
+                    initElementsAnimation(this);
+                    animateElShow(this.slides[this.params.initialSlide]);
+
+                    var btnPrev = document.querySelector('.navigation-button-prev');
+                    var btnNext = document.querySelector('.navigation-button-next');
+                    var slider = this;
+
+                    btnPrev.addEventListener('click', function () {
+                        goToPrev(slider);
+                    })
+
+                    btnNext.addEventListener('click', function () {
+                        goToNext(slider);
+                    })
+
+
                 },
-                // navigation: {
-                //     prevEl: ".navigation-button-prev",
-                //     nextEl: ".navigation-button-next"
-                // },
-                on: {
-                    init: function()
-                    {
-                        initElementsAnimation(this);
-                        animateElShow(this.slides[this.params.initialSlide + 1]);
-                    }
-                    ,
-                    slideChange: function ()
-                    {
-                        initElementsAnimation(this);
-                        activeSlide(this, this.slides, this.activeIndex, this.previousIndex);
-                    }
-                }
-            });
-
-            function initElementsAnimation(slider)
-            {
-                var self = this;
-                self.slider = slider;
-
-                console.log(self.slider.slides.length);
-
-                for(let i = 0; i < self.slider.slides.length; i++)
+                slideChange: function ()
                 {
-                    self.slider.slides[i].elements = [].slice.call(self.slider.slides[i].querySelectorAll('.animation'),0);
-
-                    for(let j = 0; j < self.slider.slides[i].elements.length; j++)
-                    {
-                        self.slider.slides[i].elements[j].style.animationDuration = self.slider.params.speed / 3 + "ms";
-                        self.slider.slides[i].elements[j].classAnimIn = self.slider.slides[i].elements[j].dataset.animatedIn;
-                        self.slider.slides[i].elements[j].classAnimOut = self.slider.slides[i].elements[j].dataset.animatedOut;
-                    }
+                    activeSlide(this, this.slides, this.activeIndex, this.previousIndex);
                 }
             }
+        });
 
-            function animateElShow(slide)
+        function goToNext(slider)
+        {
+            var s = this;
+            s.slider = slider;
+            s.nextSlider = s.slider.activeIndex + 1;
+
+            if (s.slider.activeIndex === s.slider.slides.length - 1)
             {
-                var self = this;
-                self.slide = slide;
+                s.slider.slideTo(0);
+            } else {
+                s.slider.slideTo(s.nextSlider);
+            }
+        }
 
-                for(let i = 0; i < self.slide.elements.length; i++)
+        function goToPrev(slider)
+        {
+            var s = this;
+            s.slider = slider;
+            s.prevSlider = s.slider.activeIndex - 1;
+
+            if (s.slider.activeIndex === 0)
+            {
+                s.slider.slideTo(s.slider.slides.length - 1);
+            } else {
+                s.slider.slideTo(s.prevSlider);
+            }
+        }
+
+        function initElementsAnimation(slider)
+        {
+            var self = this;
+            self.slider = slider;
+
+            for(let i = 0; i < self.slider.slides.length; i++)
+            {
+                self.slider.slides[i].elements = [].slice.call(self.slider.slides[i].querySelectorAll('.animation'),0);
+
+                for(let j = 0; j < self.slider.slides[i].elements.length; j++)
                 {
-                    self.slide.elements[i].classList.remove(self.slide.elements[i].classAnimOut);
-                    self.slide.elements[i].classList.add('animated');
-                    self.slide.elements[i].classList.add(self.slide.elements[i].classAnimIn);
+                    self.slider.slides[i].elements[j].style.animationDuration = self.slider.params.speed / 3 + "ms";
+                    self.slider.slides[i].elements[j].classAnimIn = self.slider.slides[i].elements[j].dataset.animatedIn;
+                    self.slider.slides[i].elements[j].classAnimOut = self.slider.slides[i].elements[j].dataset.animatedOut;
                 }
             }
+        }
 
-            function animateElHide(slide)
+        function animateElShow(slide)
+        {
+            var self = this;
+            self.slide = slide;
+
+            for(let i = 0; i < self.slide.elements.length; i++)
             {
-                var self = this;
-                self.slide = slide;
+                self.slide.elements[i].classList.remove(self.slide.elements[i].classAnimOut);
+                self.slide.elements[i].classList.add('animated');
+                self.slide.elements[i].classList.add(self.slide.elements[i].classAnimIn);
+            }
+        }
 
-                for(let i = 0; i < self.slide.elements.length; i++)
-                {
-                    self.slide.elements[i].classList.remove(self.slide.elements[i].classAnimIn);
-                    self.slide.elements[i].classList.add('animated');
-                    self.slide.elements[i].classList.add(self.slide.elements[i].classAnimOut);
-                }
+        function animateElHide(slide)
+        {
+            var self = this;
+            self.slide = slide;
+
+            for(let i = 0; i < self.slide.elements.length; i++)
+            {
+                self.slide.elements[i].classList.remove(self.slide.elements[i].classAnimIn);
+                self.slide.elements[i].classList.add('animated');
+                self.slide.elements[i].classList.add(self.slide.elements[i].classAnimOut);
+            }
+        }
+
+        function activeSlide(slider,slides,ind,prev)
+        {
+            var self = this;
+            self.ind = ind;
+            self.prev = prev;
+            self.slider = slider;
+            self.slides = slides;
+
+            if(self.prev !== undefined)
+            {
+                animateElHide(self.slides[self.prev]);
             }
 
-            function activeSlide(slider,slides,ind,prev)
-            {
-                var _s = this;
-                _s.ind = ind;
-                _s.prev = prev;
-                _s.slides = slides;
-
-                if(_s.prev !== undefined)
-                {
-                    animateElHide(_s.slides[_s.prev]);
-                }
-
-                console.log(slider.params);
-
-                setTimeout(function () {
-                    animateElShow(_s.slides[_s.ind]);
-                },slider.params.speed / 3);
-            }
+            setTimeout(function () {
+                animateElShow(self.slides[self.ind]);
+            },self.slider.params.speed / 3);
         }
     }
 
-    initMainSlider()
+    if (document.querySelector('.banner-slider'))
+    {
+        initMainSlider()
+    }
 })
 
